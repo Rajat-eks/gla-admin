01bbb72036dacd738b606acd13fa2c7d10a44be9
Rajat Verma <rajat.verma@effectualservices.in>
2025-08-28 09:16:57 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+   preview: {
+    port: 5000, // or whatever port you're using
+    host: true, // allow external access
+    allowedHosts: ['portal.staneffect.ai'], // 👈 add your domain here
+  },
 });
